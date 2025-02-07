ca641d53f21747ba136ce60f51118df399800e84
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2025-02-07 23:04:48 +0530

    feat(add): User and Event Schema

---
+++ models/Event.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: Date, required: true },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+});
+
+export const Event = mongoose.model('Event', eventSchema);
