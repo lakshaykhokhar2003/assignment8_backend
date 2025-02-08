f1734d3f5c7b11ea0df558b456f5edaed1784395
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2025-02-08 14:27:31 +0530

    fix: error in jwt verification

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const jwtAuth = (req, res, next) => {
-    const token = req.header('Authorization');
+    const token = req.header('Authorization')?.split(' ')[1];
     if (!token) return res.status(401).send('Access denied');
 
     try {
